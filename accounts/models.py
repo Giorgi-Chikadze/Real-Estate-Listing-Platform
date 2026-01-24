3b06275525462734846474845ca203ae0b44841f
Giorgi <giorgitchikadze200@gmail.com>
2026-01-24 21:11:42 +0400

    created accounts and booking apps

---
+++ accounts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class AgentProfile(models.Model):
+    user = models.OneToOneField(User, related_name="agents", on_delete=models.CASCADE)
+    phone = models.PositiveIntegerFieldIntegerField(unique=True)
+    agency_name = models.CharField(max_length=255, unique=True)
+    bio = models.TextField()
+    photo = models.ImageField(upload_to="media/properties")
+    is_verified = models.BooleanField()
+
+
+
+    
+
+
+
+
---
+++ booking/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class VievingBooking(models.Model):
+
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected'),
+    ]
+
+
+    user = models.ForeignKey("User", related_name="booking", on_delete=models.CASCADE)
+    property = models.ForeignKey("Property", related_name="booked_property", on_delete=models.CASCADE)
+    date = models.DateTimeField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    
