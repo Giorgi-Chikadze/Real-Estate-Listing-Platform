ec874400e1eb1e35f1172ab0133e2bf82298634a
babadu14 <andriamanagadze7@gmail.com>
2026-02-08 00:31:10 +0100

    added serializers and views

--- properties/serializers.py
+++ properties/serializers.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
 from properties.models import Property, PropertyImage, Amenity
 
-class PropertyLIstSerializer(serializers.ModelSerializer):
-    city_name = serializers.CharField(source='city.name')
+class PropertyListSerializer(serializers.ModelSerializer):
+    city_name = serializers.CharField(source='city.name', read_only=True, allow_null=True)
     area_name = serializers.CharField(source='area.name', read_only=True, allow_null=True)
     primary_image = serializers.SerializerMethodField()
     
     class Meta:
-        model = property
+        model = Property
         fields = ['title', 'price', 'property_type', 
                   'status','bedrooms','bathrooms','area_sq_m',
-                  'created_at']
+                  'created_at', 'city_name', 'area_name', 'primary_image']
         read_only_fields = ['created_at']
 
     def get_primary_image(self,obj):
@@ -22,3 +22,25 @@ class PropertyLIstSerializer(serializers.ModelSerializer):
             return first_image.image.url
     
         return None
+
+
+class AmenitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Amenity
+        fields = ['id','name']
+
+class PropertyImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PropertyImage
+        fields = ['id', 'image']
+
+class PropertyDetailSerializer(serializers.ModelSerializer):
+    city_name = serializers.CharField(source='city.name')
+    images = PropertyImageSerializer(many=True, read_only=True)
+    amenities = AmenitySerializer(many=True, read_only=True)
+    area_name = serializers.CharField(source='area.name', read_only=True, allow_null=True)
+    
+    class Meta:
+        model = Property
+        fields = ['agent','title', 'price', 'description',
+                   'property_type','address','area','city','amenities', 'images']
--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Property, PropertyImage, Amenity
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+class PropertyMOdelviewset(viewsets.ModelViewSet):
+    queryset = Property.objects.all()
+    filter_backends = DjangoFilterBackend
+    search_fields = ['title', 'address']
+    ordering_fields = ['price']
+
+    
